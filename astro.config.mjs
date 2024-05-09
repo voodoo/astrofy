3f885eca43c15323812649f1a1ba845e56f8fcbd
Paul Vudmaska <paul@vudmaska.com>
2024-05-09 12:12:32 -0500

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://voodoo.github.io',
-  base: 'astrofy',
+  base: '/astrofy',
   integrations: [mdx(), sitemap(), tailwind()]
 });
